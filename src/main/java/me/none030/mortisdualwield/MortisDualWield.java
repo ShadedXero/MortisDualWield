a3570f9f8ddba8313e89b516f18a96feb3f0c90c
None030 <muhammadammad030@gmail.com>
2023-06-04 18:16:50 +0500

    Add files via upload

---
+++ src/main/java/me/none030/mortisdualwield/MortisDualWield.java
@@ -0,0 +1,17 @@
+package me.none030.mortisdualwield;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MortisDualWield extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
